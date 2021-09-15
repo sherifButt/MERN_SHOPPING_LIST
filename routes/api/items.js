e35086cf8e12fa9c5e53ee7f4a16fc558548fe24
sherifButt <sherif.butt@gmail.com>
2021-09-15 19:05:57 +0100

    add get api/items router and post api/items router

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,9 +4,33 @@ const router = express.Router();
 // Item Model
 const Item = require('../../models/Item')
 
-router.get('/', (req, res) => {
-   const body = req.body;
-   res.json({body})
+// @route GET api/items
+// @desc Get all items
+// @access Public
+router.get('/', async (req, res) => {
+   try {
+      const items = await Item.find().sort({ date: -1 })
+      res.json({ items })
+   } catch (err) {
+      console.log(`Error geting data form DB`)
+   }
+})
+
+// @route POST api/item
+// @desc Create a Post object
+// @access Public
+router.post('/', async (req, res) => {
+   try {
+      const newItems = new Item({
+         name: req.body.name
+      })
+       
+      const savedItem = await newItems.save()
+      res.json({ savedItem })
+      
+   } catch (err) {
+      console.log(`Error geting data form DB`)
+   }
 })
 
 module.exports = router;
