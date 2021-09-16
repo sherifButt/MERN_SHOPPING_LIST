f8adfa917a41b87ad12afe9495ff25207161bfb6
sherifButt <sherif.butt@gmail.com>
2021-09-16 10:23:49 +0100

    add client/src/compnents/AppNavbar.js & install reactstrap

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AppNavbar from './components/AppNavbar'
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
