5f0fee190e192ae549912514d916efe4deb23d97
sherifButt <sherif.butt@gmail.com>
2021-09-20 17:19:08 +0100

    add drag and drop function to item list

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Button, Container, ListGroup, ListGroupItem } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // COMPONNETNT
 import ItemModal from './ItemModal';
@@ -10,40 +11,69 @@ import { useSelector, useDispatch } from 'react-redux';
 import { deleteItem, getItems } from '../redux/actions/itemActions';
 
 const ShoppingList = () => {
-   
    const items = useSelector(state => state.item.items);
-   // const [_items,set_Items] = useState({})
    const dispatch = useDispatch();
-   
-   console.log('=>',items);
-   useEffect( () => {
-       dispatch(getItems());
+
+   console.log('=>', items);
+   useEffect(() => {
+      dispatch(getItems());
    }, []);
 
    return (
-      <Container>
-         <ItemModal buttonLabel="Add Item" />
-         <ListGroup>
-            <TransitionGroup className="shopping-list">
-               {items.map(({ _id, name }) => (
-                  <CSSTransition key={_id} timeout={500} classNames="fade">
-                     <ListGroupItem>
-                        <Button
-                           className="remove-btn"
-                           color="danger"
-                           size="sm"
-                           onClick={() => {
-                              dispatch(deleteItem(_id));
-                           }}>
-                           &times;
-                        </Button>
-                        {name}
-                     </ListGroupItem>
-                  </CSSTransition>
-               ))}
-            </TransitionGroup>
-         </ListGroup>
-      </Container>
+      <DragDropContext
+         onDragEnd={(...props) => {
+            console.log(props);
+         }}>
+         <Container>
+            <ItemModal buttonLabel="Add Item" />
+            <ListGroup>
+               <TransitionGroup className="shopping-list">
+                  <Droppable droppableId="droppable-1" type="Items">
+                     {(provided, snapshot) => (
+                        <div ref={provided.innerRef} {...provided.droppableProps}>
+                           {items.map(({ _id, name }, i) => (
+                              <CSSTransition key={_id} timeout={500} classNames="fade">
+                                 <Draggable key={i} draggableId={'draggable-1' + i} index={i}>
+                                    {(provided, snapshot) => (
+                                       <div
+                                          ref={provided.innerRef}
+                                          {...provided.draggableProps}
+                                          style={{
+                                             ...provided.draggableProps.style,
+                                             boxShadow: snapshot.isDragging
+                                                ? '0 0 .9rem #66666640'
+                                                : 'none',
+                                             borderRadius: snapshot.isDragging ? '5px' : 'none',
+                                          }}>
+                                          <ListGroupItem className="mb-2">
+                                             <span {...provided.dragHandleProps} color="light">
+                                                {' '}
+                                                =
+                                             </span>
+                                             <Button
+                                                className="remove-btn ml-3"
+                                                color="danger"
+                                                size="sm"
+                                                onClick={() => {
+                                                   dispatch(deleteItem(_id));
+                                                }}>
+                                                &times;
+                                             </Button>
+                                             {name}
+                                          </ListGroupItem>
+                                       </div>
+                                    )}
+                                 </Draggable>
+                              </CSSTransition>
+                           ))}
+                           {provided.placeholder}
+                        </div>
+                     )}
+                  </Droppable>
+               </TransitionGroup>
+            </ListGroup>
+         </Container>
+      </DragDropContext>
    );
 };
 
