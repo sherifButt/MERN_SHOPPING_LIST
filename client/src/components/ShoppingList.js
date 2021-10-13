d99bf265d71fd96dbecab048137d32cda18218d9
sherifButt <sherif.butt@gmail.com>
2021-10-13 22:45:09 +0100

    added drag & drop authentication required

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -3,13 +3,14 @@ import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 // REDUX
 import { connect } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { Tooltip,Button, Container, ListGroup, ListGroupItem, Badge, CustomInput } from 'reactstrap';
+import { Tooltip,Button, Container, ListGroup, ListGroupItem, Badge, CustomInput, Alert } from 'reactstrap';
 import {
    deleteItem,
    getItems,
    itemDndReOrder,
    itemDndReArrange,
 } from '../redux/actions/itemActions';
+import { returnErrors } from '../redux/actions/errorActions'
 // COMPONNETNT
 import ItemModal from './ItemModal';
 
@@ -21,8 +22,8 @@ const ShoppingList = ({
    isAuthenticated,
    itemDndReOrder,
    itemDndReArrange,
+   returnErrors,
 }) => {
-   
    useEffect(() => {
       getItems();
    }, []);
@@ -62,17 +63,24 @@ const ShoppingList = ({
          onDragEnd={param => {
             const srcI = param.source.index;
             const desI = !!param.destination ? param.destination.index : null;
-          
-            if ( desI !== null && isAuthenticated) {
+
+            if (desI !== null && isAuthenticated) {
                console.log(items[srcI]._id, desI);
                console.log(items[desI]._id, srcI);
                itemDndReArrange({
                   item0: { _id: items[srcI]._id, srcI: srcI, desI: desI },
                   item1: { _id: items[desI]._id, srcI: desI, desI: srcI },
                });
-            } 
+            } else {
+               returnErrors('Please log in to re-arrange items...', 403, 'D&D_AUTH_ERROR');
+            }
          }}>
          <Container>
+            {error.id === 'D&D_AUTH_ERROR' ? (
+               <Alert color="danger">{error.msg}</Alert>
+            ) : (
+               ''
+            )}
             <ItemModal buttonLabel="Add Item" />
             <ListGroup>
                <TransitionGroup className="shopping-list">
@@ -80,7 +88,7 @@ const ShoppingList = ({
                      {(provided, snapshot) => (
                         <div ref={provided.innerRef} {...provided.droppableProps}>
                            {items.map(({ _id, name, category_id, description, order }, i) => (
-                              <Draggable key={_id} draggableId={'draggable-1'+_id} index={i}>
+                              <Draggable key={_id} draggableId={'draggable-1' + _id} index={i}>
                                  {(provided, snapshot) => (
                                     <div
                                        // key={i + _id}
@@ -142,6 +150,10 @@ const mapStateToProps = state => ({
    error: state.error,
    isAuthenticated: state.auth.isAuthenticated,
 });
-export default connect(mapStateToProps, { getItems, deleteItem, itemDndReOrder, itemDndReArrange })(
-   ShoppingList
-);
+export default connect(mapStateToProps, {
+   getItems,
+   deleteItem,
+   itemDndReOrder,
+   itemDndReArrange,
+   returnErrors,
+})(ShoppingList);
